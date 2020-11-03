23ea4e29c979a5c8346e35781d537661f417ad6d
DenysSemerych <d.semerych@smartweb.com.ua>
2020-11-03 17:19:13 +0200

    Initial Commit

---
+++ ChatDS/ChatDSApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatDSApp.swift
+//  ChatDS
+//
+//  Created by Денис Семерич on 03.11.2020.
+//
+
+import SwiftUI
+
+@main
+struct ChatDSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
